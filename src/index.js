9be6bba6b13b3245a20378037af1e1c5b9ce008d
JaimeGuarino <jakueguarino@gmail.com>
2019-07-18 16:38:14 +0200

    Learning to use Classes. Next season, Components 1st Lesson

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-// Spread Operator
-// const first= [1, 2, 3];
-// const second= [4, 5, 6];
+// Classes
 
-// // const combined = first.concat(second); // ambas expresiones hacen o mismo con la funcion spread ...
-// // const combined = [...first,'a', ...second 'b']; // si quiero meter un elemento en medio, se pone en medio un ,'x', siendo x el elemento que queremos meter, y si queremmos añadirlo al final de igual manera . 
+// const person = {
+//     name: "Mosh",
+   
+// };
 
-// const clone = [... first];
-// console.log(first); // en consola aparece lo mismo dos veces
-// console.log(clone);
+// const person2 = {               // Problema: La implementacion de walk es la misma
+//     name: "Mosh",                //Solución: Aplicar Clases
+//     walk () {
+//         console.log("walk");
+//     }
+// };
 
-const first = { name: 'Mosh'};
-const second= { job: 'Instructor'};
+import  Teacher, { promote } from "./teacher";  // Al exportar la función promote en teacher, nos sale como opción
+// Deafult -> import... from ''; 
+// Named -> import {...} from '';
 
-const combined = {...first, ...second, location: 'Australia'}; 
-console.log(combined); // Obtenemos en consola the name, job y se le añade la location:
+ import React, { Component } from 'react'; // Component se usará la mayoría de las veces.
 
-// Spread Operator for clone an object
+const teacher = new Teacher ("Mosh","MSc");
 
- const clone = {...first,...second};
- console.log(clone);
+teacher.teach(); 
--- src/teacher.js
+++ src/teacher.js
@@ -1,7 +1,10 @@
 // Para hacer visible este modulo se pone un export para poder usarlo desde otros files
 import {Person} from "./person"; //importamos la clase Person desde el file person.js
 
-export class Teacher extends Person {
+export function promote() {};
+
+export  default class Teacher extends Person {  // al añadir default, por defecto, lo que se importara es teacher, por lo que no haría falta poner import teacher entre corchetes
+
   constructor(name, degree) {
     super(name);
     this.degree = degree;
