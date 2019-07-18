815bb6a1914d337e8cb8829e8f2052cfa3a24d25
JaimeGuarino <jakueguarino@gmail.com>
2019-07-18 15:56:19 +0200

    Learning Spread Operator: Combined and Clone

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-// Objet Destructuring 
-const address= {
-    street: '',
-    city: '',
-    country: ''
-}
+// Spread Operator
+// const first= [1, 2, 3];
+// const second= [4, 5, 6];
 
-const street = address.street; 
-const city = address.city; 
-const country = address.country; 
+// // const combined = first.concat(second); // ambas expresiones hacen o mismo con la funcion spread ...
+// // const combined = [...first,'a', ...second 'b']; // si quiero meter un elemento en medio, se pone en medio un ,'x', siendo x el elemento que queremos meter, y si queremmos añadirlo al final de igual manera . 
 
-//const {street, city, country}= address; // En esta línea tenemos el mismo resultado que las 3 anteriores
+// const clone = [... first];
+// console.log(first); // en consola aparece lo mismo dos veces
+// console.log(clone);
 
-const { street: st } = address; // Forma que se usará más amenudo. 
+const first = { name: 'Mosh'};
+const second= { job: 'Instructor'};
+
+const combined = {...first, ...second, location: 'Australia'}; 
+console.log(combined); // Obtenemos en consola the name, job y se le añade la location:
+
+// Spread Operator for clone an object
+
+ const clone = {...first,...second};
+ console.log(clone);
