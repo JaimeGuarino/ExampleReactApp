3031167fdb8303af253a312071e265736f7c1ab2
JaimeGuarino <jakueguarino@gmail.com>
2019-07-18 14:43:13 +0200

    Arrow functions with other examples

--- src/index.js
+++ src/index.js
@@ -13,3 +13,22 @@ person.walk();
 
 const walk = person.walk.bind(person); // el argumento que recibe, será el nuevo valor que asignaremosa  this
 walk();
+
+// Arrow functions// 
+
+//const square = function(number){            // Las dos líneas de arriba son las mismas que las de abajo
+    //return number * number;                 // Las de abajo está puesto de una forma mas simple y elegante 
+//}                                           // gracias a las arrow function
+
+const square = number => number * number
+console.log(square(5));
+// Otro ejemlo como el anterior
+//
+   // {id: 1, isActive: true},
+    //{id: 2, isActive: true},
+    //{id: 3, isActive: false},
+//];
+
+//const activeJobs = jobs.filter(function(job) {return job.isActive; }); // Es lo mismo
+//const activeJobs = jobs.filter(job => jobs.isActive);
+
