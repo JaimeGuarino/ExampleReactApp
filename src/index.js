a72e1bfcb2498e4dcd7c1ddec2bdf2b37bbda706
joaquin caceres galan <jcg230795@gmail.com>
2019-07-19 11:29:04 +0200

    6.Arrow Functions

--- src/index.js
+++ src/index.js
@@ -19,6 +19,47 @@ const person ={
 person.talk();
 person.name = '';
 
+
+
+const person= {
+    name: "Mosh",
+    walk(){
+        console.log(this)
+    }
+}
+
+person.walk();
+
+const walk = person.walk.bind(person);
+walk();
+
 */
+
+//6. Arrow Functions
+
+/*
+
+const square = function(number){
+    return number * number;
+}
+
+*/
+
+const square = number => number * number;
+console.log(square(5))
+
+
+const jobs = [
+
+    {id:1, isActive: true},
+    {id:2, isActive: true},
+    {id:3, isActive: false},
+]
+
+const activeJobs =
+
+
+
+
 
 
